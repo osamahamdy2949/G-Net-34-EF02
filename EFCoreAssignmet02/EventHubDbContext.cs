2261329b68da013f9599cd8fe99d66584292f47d
osamahamdy2949 <osama.hamdy2949@gmail.com>
2026-04-14 16:28:09 +0200

    Create DbContext

---
+++ EFCoreAssignmet02/EventHubDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreAssignmet02
+{
+    internal class EventHubDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;DataBase = EventHubDb;Trusted_Connection = true;TrustServerCertificate = true");
+        }
+    }
+}
