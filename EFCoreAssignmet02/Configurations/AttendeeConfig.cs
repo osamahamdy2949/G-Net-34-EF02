571ac6181cc74a700be32cbd2edb601fd36b535a
osamahamdy2949 <osama.hamdy2949@gmail.com>
2026-04-19 18:48:21 +0200

    Revert "Add Owend Prop To Attendee"
    This reverts commit 1dad9da2030da0acc5217292339da6e932ad1b31.

--- EFCoreAssignmet02/Configurations/AttendeeConfig.cs
+++ EFCoreAssignmet02/Configurations/AttendeeConfig.cs
@@ -18,8 +18,6 @@ namespace EFCoreAssignmet02.Configurations
                    .HasForeignKey<Badge>(b => b.AttendeeId)
                    .IsRequired();
 
-            builder.OwnsOne(a => a.HomeAddress);
-
             //builder.HasMany(e => e.Events)
             //       .WithMany(a => a.Attendees)
             //       .UsingEntity(jt =>
