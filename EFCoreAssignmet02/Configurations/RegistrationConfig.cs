07e80664d770b25d9194f0889fa701cb937eaadc
osamahamdy2949 <osama.hamdy2949@gmail.com>
2026-04-19 18:49:27 +0200

    Revert "Final Solution"
    This reverts commit 99b08a8a3e50b9b0e870fd1716f8488c88b3535b.

--- EFCoreAssignmet02/Configurations/RegistrationConfig.cs
+++ EFCoreAssignmet02/Configurations/RegistrationConfig.cs
@@ -16,8 +16,7 @@ namespace EFCoreAssignmet02.Configurations
         {
             builder.HasKey(r => new { r.EventId, r.AttendeeId });
 
-            builder.Property(r => r.RegistrationDate).HasColumnName("FirstCreationAt")
-                                                     .HasDefaultValueSql("GETDATE()");
+            builder.Property(r => r.RegistrationDate).HasColumnName("FirstCreationAt").HasDefaultValueSql("GETDATE()");
         }
     }
 }
